ba1951ce5b9bd42f573d50fae246a7043f2d5eef
Acosta Gustavo <gustavo080487@gmail.com>
2025-06-28 17:20:18 -0300

    layout de productos

--- app/products/layout.tsx
+++ app/products/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
           <CategorySidebar />
         </aside>
       </div>
-      <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+      <div className="flex-grow p-6  md:p-12">{children}</div>
     </div>
   );
 }
