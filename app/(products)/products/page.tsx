7f42b3d270c093ffff6b10de5dd41bb6e577f596
Acosta Gustavo <gustavo080487@gmail.com>
2025-08-20 17:45:03 -0300

    setting componet server

--- app/(products)/products/page.tsx
+++ app/(products)/products/page.tsx
@@ -1,31 +1,42 @@
-"use client"
 
 import ProductGrid from '@/components/products/product-grid'
 import { getProductos } from '@/lib/actions'
 import { Product } from '@/lib/definitions';
 import { Console } from 'console'
-import { useEffect, useState } from "react"
+import { notFound } from 'next/navigation';
+//import { useEffect, useState } from "react"
 
-export default function page() {
 
-    const [products, setProducts] = useState<Array<Product>>([]);
-  
-      useEffect(() => {
-        async function fetchData() {
-          const data = await getProductos();
-          setProducts(data as Product[]);
-        }
-        fetchData();
-      }, []);
-      
-    // console.log(products)
 
-  //const products = await getProductos()
-
-  return (
-    <main className="container mx-auto px-4 py-6">
-      <h1 className="text-3xl font-bold mb-6 text-center">Productos Destacados</h1>
-      <ProductGrid products={products} />
-    </main>
-  )
+export default async function ProductItemPage() {
+  const product = await getProductos()
+  if (!product || product.length === 0) {
+    notFound()
+  }
+  return <ProductGrid products={product} />
 }
+
+
+// export default function page() {
+
+//     const [products, setProducts] = useState<Array<Product>>([]);
+  
+//       useEffect(() => {
+//         async function fetchData() {
+//           const data = await getProductos();
+//           setProducts(data as Product[]);
+//         }
+//         fetchData();
+//       }, []);
+      
+//     // console.log(products)
+
+//   //const products = await getProductos()
+
+//   return (
+//     <main className="container mx-auto px-4 py-6">
+//       <h1 className="text-3xl font-bold mb-6 text-center">Productos Destacados</h1>
+//       <ProductGrid products={products} />
+//     </main>
+//   )
+// }
