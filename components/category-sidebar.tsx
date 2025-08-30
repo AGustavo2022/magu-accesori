ada290d783dc3205267bd6893036b0869993f13f
Acosta Gustavo <gustavo080487@gmail.com>
2025-08-30 11:59:14 -0300

    factorizando codigo

--- components/category-sidebar.tsx
+++ components/category-sidebar.tsx
@@ -1,4 +1,3 @@
-
 import { getCategorias2 } from "@/lib/actions"
 import Link from "next/link"
 
@@ -10,15 +9,13 @@ import {
 } from "@/components/ui/accordion"
 import { Subcategory } from "@/lib/definitions"
 
-export default  async function CategorySidebar() {
+export default async function CategorySidebar() {
 
   const productCategories = await getCategorias2()
 
-  console.log(productCategories)
-
-return (
+  return (
     <Accordion type="single" collapsible className="w-full">
-      
+
       {productCategories.map((cat) => (
         <AccordionItem key={cat.category_id} value={`cat-${cat.category_id}`}>
           <AccordionTrigger className="w-full">
@@ -26,15 +23,15 @@ return (
           </AccordionTrigger>
           <AccordionContent className="flex flex-col gap-2 pl-4">
             {cat.subcategories.map((sub: Subcategory) => (
-                <Link
-                  key={sub.subcategory_id}
-                  href={`/category/${sub.subcategory_id}`}
-                  className="text-gray-700 hover:text-blue-600 text-sm"
-                >
-                  {sub.subcategory_name}
-                </Link>
+              <Link
+                key={sub.subcategory_id}
+                href={`/category/${sub.subcategory_id}`}
+                className="text-gray-700 hover:text-blue-600 text-sm"
+              >
+                {sub.subcategory_name}
+              </Link>
 
-              ))
+            ))
             }
           </AccordionContent>
         </AccordionItem>
