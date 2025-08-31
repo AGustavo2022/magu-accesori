5beed8dbae7a5dbe91b9bf67c914740c1d993e28
Acosta Gustavo <gustavo080487@gmail.com>
2025-08-31 09:29:58 -0300

    se agrega props al produtc_id

--- app/(products)/products/[product_id]/page.tsx
+++ app/(products)/products/[product_id]/page.tsx
@@ -4,8 +4,9 @@ import { getProductsByid } from "@/lib/actions"
 import ProductItem from "@/components/products/product-item"
 
 
-export default async function ProductItemPage({ params }: { params: { product_id: string } }) {
+export default async function ProductItemPage(props: { params: Promise<{ product_id: string }> }) {
   
+  const params = await props.params
   const numericProductId = Number(params.product_id)
   const product = await getProductsByid(numericProductId)
   if (!product || product.length === 0) {
