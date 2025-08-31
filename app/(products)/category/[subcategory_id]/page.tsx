7fbabf531a5814ec07cca1e8824b732d7c1e6ded
Acosta Gustavo <gustavo080487@gmail.com>
2025-08-31 09:17:02 -0300

    se corrigio category id

--- app/(products)/category/[subcategory_id]/page.tsx
+++ app/(products)/category/[subcategory_id]/page.tsx
@@ -1,31 +1,13 @@
-"use client"
-
 import ProductGrid from "@/components/products/product-grid"
 import { getProductsBySubcategory } from "@/lib/actions"
-import { useEffect, useState } from "react";
-import { Product } from "@/lib/definitions";
 import React from "react"
 
-export default   function CategoryPage({ params }: { params: { subcategory_id: string } }) {
+export default async function CategoryPage({ params }: { params: { subcategory_id: string } }) {
 
 
   const numericSubcategoryId = Number(params.subcategory_id);
 
-  // const products = await getProductsBySubcategory(numericSubcategoryId)
-
-  // console.log(products)
-  
-  const [products, setProducts] = useState<Array<Product>>([]);
-
-
-  useEffect(() => {
-    async function fetchData() {
-      const data = await getProductsBySubcategory(numericSubcategoryId)
-      console.log(data)
-      setProducts(data as Product[]);
-    }
-    fetchData();
-  }, [numericSubcategoryId]);
+  const products = await getProductsBySubcategory(numericSubcategoryId)
 
   return (
     <div className="container mx-auto px-4 py-8">
