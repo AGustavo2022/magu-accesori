d30532786ba7ea113c0e4584a0aae40be576776d
Acosta Gustavo <gustavo080487@gmail.com>
2025-12-05 21:01:52 -0300

    se optimizan layout

--- app/(products)/layout.tsx
+++ app/(products)/layout.tsx
@@ -1,9 +1,7 @@
 
 import CategorySidebar from "@/components/products/category-sidebar";
 import { SkeletonProductsGrid } from "@/components/skeletons";
-// import { RevenueChartSkeleton } from "@/components/skeletons";
 import {
-  ResizableHandle,
   ResizablePanel,
   ResizablePanelGroup,
 } from "@/components/ui/resizable"
@@ -12,29 +10,20 @@ import { Suspense } from "react";
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
 
-    <ResizablePanelGroup
-      direction="horizontal"
-    >
-      <ResizablePanel defaultSize={20}>
-        <div className="flex h-screen   p-6">
-          <span className="font-semibold">
-            <CategorySidebar/>
+<ResizablePanelGroup direction="horizontal">
 
-          </span>
-        </div>
-      </ResizablePanel>
+  <ResizablePanel defaultSize={18}>
+    <CategorySidebar />
+  </ResizablePanel>
 
-      <ResizablePanel defaultSize={80}>
+  <ResizablePanel defaultSize={82}>
+    <div>
+      <Suspense fallback={<SkeletonProductsGrid />}>
+        {children}
+      </Suspense>
+    </div>
+  </ResizablePanel>
 
-        <ResizablePanel>
-          <div >
-            <Suspense fallback={<SkeletonProductsGrid />}>
-            <span className="font-semibold">{children}</span>
-            </Suspense>
-          </div>
-        </ResizablePanel>
-
-      </ResizablePanel>
-    </ResizablePanelGroup>
+</ResizablePanelGroup>
   );
 }
