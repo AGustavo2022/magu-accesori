e63303ac6d6b67eaff04f361520f01183744ba30
Acosta Gustavo <gustavo080487@gmail.com>
2025-12-08 18:37:06 -0300

    se saca cacheComponents: true,

--- next.config.ts
+++ next.config.ts
@@ -1,15 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  // Reemplazo oficial del antiguo experimental.ppr
-  cacheComponents: true,
-
-  // Habilita la optimización automática de imágenes remotas en Next 16
   images: {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "i.ibb.co", // ← CORRECCIÓN IMPORTANTE
+        hostname: "i.ibb.co",
         pathname: "/**",
       },
     ],
