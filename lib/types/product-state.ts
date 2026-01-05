450617be9f99dec34934e15aebbf338c72d30457
Acosta Gustavo <gustavo080487@gmail.com>
2026-01-05 16:19:52 -0300

    se eliminan comentarios

--- lib/types/product-state.ts
+++ lib/types/product-state.ts
@@ -1,23 +1,4 @@
 
-// export type CreateProductState = {
-//   success: boolean;
-//   message?: string | null;
-//   errors: CreateProductErrors;
-//   values: Record<string, string>;
-// };
-
-// export type CreateProductErrors = {
-//   title?: string[];
-//   shortDescription?: string[];
-//   longDescription?: string[];
-//   price?: string[];
-//   stock?: string[];
-//   image_url?: string[];
-//   category?: string[];
-//   subcategory?: string[];
-//   discount?: string[];
-// };
-
 type FieldErrors = Record<string, string[]>;
 type FormValues = Record<string, string>;
 
