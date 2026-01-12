d052bb357389b57e4354a6c3d7fc38034a607645
Acosta Gustavo <gustavo080487@gmail.com>
2026-01-12 01:28:13 -0300

    se eliminan import sin uso y optimiza los params

--- app/product/[product_id]/page.tsx
+++ app/product/[product_id]/page.tsx
@@ -1,19 +1,21 @@
 import { notFound } from "next/navigation"
 import { getProductById } from "@/lib/data"
 import { ProductDetail } from "@/components/products/product-detail"
-import { Spinner } from "@/components/ui/spinner";
 
-export default async function ProductItemPage(props: { params: Promise<{ product_id: string }> }) {
-  
-  const { product_id } = await props.params;
 
-  // console.log(product_id)
+export default async function ProductItemPage({
+  params,
+}:{
+  params: Promise<{ product_id: string }>
+}) {
+
+  const { product_id } = await params;
 
   const product = await getProductById(product_id)
+  
   if (!product || product.length === 0) {
     notFound()
   }
-
 
   return (
     
