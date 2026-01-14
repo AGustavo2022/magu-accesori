1820cd2c5079e47ffd6e4f86ab8f739d917bb487
Acosta Gustavo <gustavo080487@gmail.com>
2026-01-14 18:37:44 -0300

    se modifica login

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -19,7 +19,7 @@ export default function LoginForm() {
     <form action={formAction} className="space-y-3">
       <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
         <h1 className= "mb-3 text-2xl">
-          Please log in to continue.
+          Inicie sesión para continuar.
         </h1>
         <div className="w-full">
           <div>
@@ -35,7 +35,7 @@ export default function LoginForm() {
                 id="email"
                 type="email"
                 name="email"
-                placeholder="Enter your email address"
+                placeholder="Introduce tu dirección de correo electrónico"
                 required
               />
               {/* <AtSymbolIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" /> */}
@@ -54,7 +54,7 @@ export default function LoginForm() {
                 id="password"
                 type="password"
                 name="password"
-                placeholder="Enter password"
+                placeholder="Introduce la contraseña"
                 required
                 minLength={6}
               />
