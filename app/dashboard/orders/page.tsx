7dcfdeadfd2c995adc4c5349c12de6ee9a58357f
Acosta Gustavo <gustavo080487@gmail.com>
2026-02-15 18:00:10 -0300

    se corrige searchParams y ya no es una promesa

--- app/dashboard/orders/page.tsx
+++ app/dashboard/orders/page.tsx
@@ -3,39 +3,40 @@ import { OrdersTable } from "@/components/dashboard/orders-table"
 import PaginationProducts from "@/components/pagination-products"
 import SearchNew from "@/components/search"
 import { getPendingOrdersCount } from "@/lib/actions/order.actions"
-import { getOrdersDashboardTotalCount, getOrdersPages, getOrdersTotalPages } from "@/lib/data/orders.data"
+import {
+  getOrdersDashboardTotalCount,
+  getOrdersPages,
+  getOrdersTotalPages
+} from "@/lib/data/orders.data"
 import { OrderStatus } from "@/lib/types/order.types"
 
-export const dynamic = "force-dynamic";
-
-export default async function OrdersPage({
-    searchParams,
-}: {
-  searchParams?: Promise<{
+interface OrdersPageProps {
+  searchParams?: {
     page?: string
     query?: string
     status?: OrderStatus
-  }>
-}) {
+  }
+}
 
-  const params = await searchParams;
-  const currentPage = Number(params?.page || 1)
-  const query = params?.query || ""
- 
-  const status = params?.status || "pending"
+export default async function OrdersPage({
+  searchParams,
+}: OrdersPageProps) {
+
+  const currentPage = Number(searchParams?.page ?? 1)
+  const query = searchParams?.query ?? ""
+  const status = searchParams?.status ?? "pending"
 
   const orders = await getOrdersPages(query, currentPage, status)
-
   const totalOrders = await getOrdersDashboardTotalCount(query, status)
-  
   const totalPage = await getOrdersTotalPages(query, status)
-
   const pendingCount = await getPendingOrdersCount()
 
   return (
     <div className="container mx-auto flex flex-col min-h-screen">
       <OrdersInitializer count={pendingCount} />
+
       <SearchNew placeholder="Buscar orden..." />
+
       <OrdersTable
         orders={orders}
         totalOrders={totalOrders}
@@ -51,6 +52,7 @@ export default async function OrdersPage({
           "drawer"
         ]}
       />
+
       <div className="mt-8">
         {totalPage > 1 && (
           <PaginationProducts
@@ -60,6 +62,5 @@ export default async function OrdersPage({
         )}
       </div>
     </div>
-
   )
 }
