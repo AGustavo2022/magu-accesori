a43da0d9f9ac900cb5f837f19ee05dc70e7bb3cc
Acosta Gustavo <gustavo080487@gmail.com>
2026-02-21 13:30:16 -0300

    se soucion import

--- app/(admin)/login/page.tsx
+++ app/(admin)/login/page.tsx
@@ -1,6 +1,5 @@
 
-import LoginForm from '@/components/login-form';
-import AcmeLogo from '@/components/navbars/acme-logo';
+import LoginForm from '@/components/auth/login-form';
 import { Suspense } from 'react';
  
 export default function LoginPage() {
